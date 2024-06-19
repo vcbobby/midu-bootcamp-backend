afa561b576b9eee965e523cf59ff6394714d040b
Victor Castillo <Castillovictor2461@gmail.com>
2024-06-19 04:34:35 -0400

    configuracion del get por ir

--- api/index.js
+++ api/index.js
@@ -21,13 +21,14 @@ app.get('/api/notes', (req, res) => {
 })
 
 app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const note = notes.find((note) => note.id === id)
-    if (note) {
-        res.json(note)
-    } else {
-        res.status(404).end()
-    }
+    const { id } = req.params
+    Note.findById(id)
+        .then((note) => {
+            res.json(note)
+        })
+        .catch((err) => {
+            res.status(400).end()
+        })
 })
 
 app.delete('/api/notes/:id', (req, res) => {
