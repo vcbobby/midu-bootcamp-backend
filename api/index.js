7ed7f0745d4f3d135b1f3cc49e9f3910bfa8cc5e
Victor Castillo <Castillovictor2461@gmail.com>
2024-06-19 03:22:27 -0400

    mongoose prueba arreglar ids

--- api/index.js
+++ api/index.js
@@ -15,7 +15,15 @@ app.get('/', (req, res) => {
 
 app.get('/api/notes', (req, res) => {
     Note.find({}).then((notes) => {
-        res.json(notes)
+        res.json(
+            notes.map((note) => {
+                const { _id, __v, ...restOfNotes } = note
+                return {
+                    ...restOfNotes,
+                    id: _id,
+                }
+            })
+        )
     })
 })
 
