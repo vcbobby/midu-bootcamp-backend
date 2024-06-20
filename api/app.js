abc1aaed877946e582f105ded47767ea827de8c5
Victor Castillo <Castillovictor2461@gmail.com>
2024-06-20 06:00:00 -0400

    configuracion para modificar proyecto en local

--- api/app.js
+++ api/app.js
@@ -4,20 +4,24 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const Note = require('../models/Note')
+const notFound = require('../middlewares/notFound')
+const error = require('../middlewares/error')
 
 app.use(cors())
 app.use(express.json())
-
-let notes = []
 
 app.get('/', (req, res) => {
     res.send('<h1>hola como ta tu</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-    Note.find({}).then((notes) => {
-        res.json(notes)
-    })
+app.get('/api/notes', (req, res, next) => {
+    Note.find({})
+        .then((notes) => {
+            res.json(notes)
+        })
+        .catch((err) => {
+            next(err)
+        })
 })
 
 app.get('/api/notes/:id', (req, res, next) => {
@@ -50,7 +54,7 @@ app.delete('/api/notes/:id', (req, res, next) => {
     const { id } = req.params
 
     Note.findByIdAndDelete(id)
-        .then((result) => {
+        .then(() => {
             res.status(204).end()
         })
         .catch((err) => {
@@ -58,7 +62,7 @@ app.delete('/api/notes/:id', (req, res, next) => {
         })
 })
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
     const note = req.body
 
     if (!note || !note.title || !note.body) {
@@ -71,19 +75,17 @@ app.post('/api/notes', (req, res) => {
         title: note.title,
         body: note.body,
     })
-    newNote.save().then((savedNote) => {
-        res.json(savedNote)
-    })
-})
-
-app.use((err, req, res, next) => {
-    if (err.name === 'CastError') {
-        res.status(400).json({
-            error: 'Id is malformed',
+    newNote
+        .save()
+        .then((savedNote) => {
+            res.json(savedNote)
         })
-    } else {
-        res.status(500).end()
-    }
+        .catch((err) => {
+            next(err)
+        })
 })
+app.use(notFound)
+
+app.use(error)
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const app = require('./api/index')
+const app = require('./api/app')
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
 
 module.exports = (req, res) => {
     app(req, res)
