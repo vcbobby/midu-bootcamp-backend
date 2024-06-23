2168ad01f684ac069ae12af040adc242c263a0b6
Victor Castillo <Castillovictor2461@gmail.com>
2024-06-23 06:30:49 -0400

    prueba modificacion archivo app

--- api/app.js
+++ api/app.js
@@ -15,14 +15,10 @@ app.get('/', (req, res) => {
     res.send('<h1>Backend de app de notas bootcamp midudev</h1>')
 })
 
-app.use('/api/notes', notesRouter)
-app.use('/api/users', usersRouter)
+// app.use('/api/notes', notesRouter)
+// app.use('/api/users', usersRouter)
 app.use(notFound)
 
 app.use(error)
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500).send('Something went wrong!')
-})
 
 module.exports = app
